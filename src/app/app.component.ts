727fe1399f2e9fd138410ebdb706d6bdf84c964b
Pablo Gonzalez <jp.gonzalez3098@gmail.com>
2025-06-29 20:07:55 -0600

    Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'Frontend';
+  datosDelFormulario: any;
+  
+  constructor(private router: Router) {}
+  
+  hrefTabla(){
+    this.router.navigate(['/tabla'], {
+      state: { datos: this.datosDelFormulario }
+    });
+  }
+
+  hrefProducto(){
+    this.router.navigate(['/'], {
+      state: { datos: this.datosDelFormulario }
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AuthGuardGuard } from './core/guard/auth-guard.guard';
+import { AuthenticatedGuardGuard } from './core/guard/authenticated-guard.guard';
+import TablaProductComponent from './tabla.product/tabla.product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login.component'),
+        canActivate: [AuthenticatedGuardGuard]
+
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./home/home.component'),
+        canActivate: [AuthGuardGuard]
+    },
+    {   path: 'tabla',
+        component: TablaProductComponent,
+        canActivate: [AuthGuardGuard] 
+    }
+];
